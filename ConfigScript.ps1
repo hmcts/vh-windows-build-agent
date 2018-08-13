c683df5133e4324e577a5fbc23dd681b1c608143
janisBerz <janis.berzins@hmcts.net>
2018-08-13 08:43:32 +0100

    Initial commit

--- ConfigScript.ps1
+++ ConfigScript.ps1
@@ -1,19 +1,24 @@
-<#
-### Install PowerShell 6
-Write-Output "Installing PS 6"
-#Set-ExecutionPolicy Unrestricted -Force
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-#Default workspace location
-Set-Location C:\
-$source = "https://github.com/PowerShell/PowerShell/releases/download/v6.0.3/PowerShell-6.0.3-win-x64.msi"
-$destination = "D:\PowerShell-6.0.3-win-x64.msi"
-$client = new-object System.Net.WebClient
-$cookie = "oraclelicense=accept-securebackup-cookie"
-$client.Headers.Add([System.Net.HttpRequestHeader]::Cookie, $cookie)
-$client.downloadFile($source, $destination)
-$proc = Start-Process -FilePath $destination -ArgumentList "/q" -Wait -PassThru
-$proc.WaitForExit()
-#>
+
+function Get-InstallMedia {
+    [CmdletBinding()]
+    param (
+        $source,
+        $destination
+    )
+    
+    begin {
+        $client = new-object System.Net.WebClient
+        #$client.Headers.Add([System.Net.HttpRequestHeader]::Cookie, $cookie)
+        $client.downloadFile($source, $destination)
+    }
+    
+    process {
+        $client.downloadFile($source, $destination)
+    }
+    
+    end {
+    }
+}
 
 ### Download and Install Java
 Write-Output "Installing Java"
@@ -33,31 +38,14 @@ $proc.WaitForExit()
 [System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";c:\Program Files\Java\jdk1.8.0_131\bin", "Machine")
 $Env:Path += ";c:\Program Files\Java\jdk1.8.0_131\bin"
 #Disable git credential manager, get more details in https://support.cloudbees.com/hc/en-us/articles/221046888-Build-Hang-or-Fail-with-Git-for-Windows
-#git config --system --unset credential.helper
-
-<#### Install Maven
-Write-Output "Installing Maven"
-$source = "https://archive.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.zip"
-$destination = "D:\maven.zip"
-$webClient = New-Object System.Net.WebClient
-$webClient.DownloadFile($source, $destination)
-$shell_app=new-object -com shell.application
-$zip_file = $shell_app.namespace($destination)
-mkdir 'C:\Program Files\apache-maven-3.5.2'
-$destination = $shell_app.namespace('C:\Program Files')
-$destination.Copyhere($zip_file.items(), 0x14)
-[System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";C:\Program Files\apache-maven-3.5.2\bin", "Machine")
-$Env:Path += ";C:\Program Files\apache-maven-3.5.2\bin"
-#>
-
 
 ### Install Git
 Write-Output "Installing Git"
 $source = "https://github.com/git-for-windows/git/releases/latest"
-$latestRelease = Invoke-WebRequest -UseBasicParsing $source -Headers @{"Accept"="application/json"}
+$latestRelease = Invoke-WebRequest -UseBasicParsing $source -Headers @{"Accept" = "application/json"}
 $json = $latestRelease.Content | ConvertFrom-Json
 $latestVersion = $json.tag_name
-$versionHead = $latestVersion.Substring(1, $latestVersion.IndexOf("windows")-2)
+$versionHead = $latestVersion.Substring(1, $latestVersion.IndexOf("windows") - 2)
 $source = "https://github.com/git-for-windows/git/releases/download/v${versionHead}.windows.1/Git-${versionHead}-64-bit.exe"
 $destination = "D:\Git-${versionHead}-64-bit.exe"
 $webClient = New-Object System.Net.WebClient
@@ -94,3 +82,60 @@ Set-Service ssh-agent -StartupType Automatic
 #& $env:SystemRoot\\System32\\Sysprep\\Sysprep.exe /oobe /generalize /quiet /quit
 #Write-Output "Waiting for VM to be ready"
 #while($true) { $imageState = Get-ItemProperty HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Setup\\State | Select ImageState; if($imageState.ImageState -ne 'IMAGE_STATE_GENERALIZE_RESEAL_TO_OOBE') { Write-Output $imageState.ImageState; Start-Sleep -s 10  } else { break } }
+
+Write-Output "Installing MS Build tools 2017..."
+
+#$VS_CHANNEL_URI = "https://aka.ms/vs/15/release/799c44140/channel"
+$VS_BUILDTOOLS_URI = "https://aka.ms/vs/15/release/vs_buildtools.exe"
+#$VS_BUILDTOOLS_SHA256 = "FA29EB83297AECADB0C4CD41E54512C953164E64EEDD9FB9D3BF9BD70C9A2D29"
+
+# Download log collection utility
+Invoke-WebRequest -Uri "https://aka.ms/vscollect.exe" -OutFile D:\collect.exe
+
+# Download vs_buildtools.exe
+Invoke-WebRequest -Uri $VS_BUILDTOOLS_URI -OutFile D:\vs_buildtools.exe
+#if ((Get-FileHash -Path C:\vs_buildtools.exe -Algorithm SHA256).Hash -ne $env:VS_BUILDTOOLS_SHA256) { throw 'Download hash does not match' }
+
+# https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-build-tools
+# Install Visual Studio Build Tools
+$p = Start-Process -Wait -PassThru -FilePath D:\vs_buildtools.exe -ArgumentList '--quiet --nocache --wait --installPath C:\BuildTools';
+if ($ret = $p.ExitCode) { D:\collect.exe; throw ('Install failed with exit code 0x{0:x}' -f $ret) }
+
+# Install VS woarkloads
+Write-Output "Installing Visual studio workloads..."
+$p = Start-Process -Wait -PassThru -FilePath D:\vs_buildtools.exe -ArgumentList "modify --quiet --nocache --wait --installPath C:\BuildTools",
+"--add Microsoft.VisualStudio.Workload.MSBuildTools",
+"--add Microsoft.Net.Core.Component.SDK",
+"--add Microsoft.Net.ComponentGroup.4.6.2.DeveloperTools",
+"--add Microsoft.VisualStudio.Workload.WebBuildTools",
+"--add Microsoft.VisualStudio.Workload.NodeBuildTools",
+"--add Microsoft.Net.Component.3.5.DeveloperTools",
+"--add Microsoft.VisualStudio.Component.TestTools.BuildTools",
+"--add Microsoft.VisualStudio.Component.TypeScript.2.8",
+"--add Microsoft.VisualStudio.Component.TestTools.BuildTools",
+"--add Microsoft.VisualStudio.Workload.MSBuildTools"; 
+if ($ret = $p.ExitCode) { D:\collect.exe; throw ('Install failed with exit code 0x{0:x}' -f $ret) }
+
+Write-Output "Creating adding MSBuild to path variable"
+[System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";C:\BuildTools\MSBuild\15.0\Bin", "Machine")
+
+Write-Output "Installing .Net Core 2.1.3..."
+$source = "https://download.microsoft.com/download/4/0/9/40920432-3302-47a8-b13c-bbc4848ad114/dotnet-sdk-2.1.302-win-x64.exe"
+$destination = "D:\dotnet-sdk-2.1.302-win-x64.exe"
+$client = new-object System.Net.WebClient
+$cookie = "oraclelicense=accept-securebackup-cookie"
+$client.Headers.Add([System.Net.HttpRequestHeader]::Cookie, $cookie)
+$client.downloadFile($source, $destination)
+$proc = Start-Process -FilePath $destination -ArgumentList "/Install /q" -Wait -PassThru
+$proc.WaitForExit()
+
+Write-Output "Installing chocolety..."
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+Write-Output "Installing nuget..."
+choco install -y nuget.commandline 
+
+Write-Output "Installing .Net 4.6.2..."
+choco install -y netfx-4.6.2-devpack
+choco install -y dotnet4.6.2
