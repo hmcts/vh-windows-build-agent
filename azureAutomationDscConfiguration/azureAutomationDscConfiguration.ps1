89064b08e6a24fd263d42684c5026938de8f4cb1
janisBerz <janis.berzins@hmcts.net>
2019-07-24 19:13:27 +0300

    New scripts for uploading dsc node config

---
+++ azureAutomationDscConfiguration/azureAutomationDscConfiguration.ps1
@@ -0,0 +1,31 @@
+
+function Add-DcsConfiguration {
+    [CmdletBinding()]
+    param (
+        $ResourceGroupName,
+        $AutomationAccountName,
+        $SourcePath = "C:\source\vh-windows-build-agent\FileResourceDemo.ps1"
+    )
+
+    begin {
+        $ConfigurationName = $SourcePath.Split("\")[-1].Replace(".ps1", "")
+    }
+
+    process {
+        Import-AzAutomationDscConfiguration -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -SourcePath $SourcePath -Force -Published
+        $CompilationJob = Start-AzAutomationDscCompilationJob -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -ConfigurationName $ConfigurationName
+
+        while ($null -eq $CompilationJob.EndTime -and $null -eq $CompilationJob.Exception) {
+            $CompilationJob = $CompilationJob | Get-AzAutomationDscCompilationJob
+            Write-Output "Compiling..."
+            Start-Sleep -Seconds 3
+        }
+
+        $CompilationJob | Get-AzAutomationDscCompilationJobOutput -Stream Any
+    }
+    end {
+    }
+}
+
+
+Add-DcsConfiguration -ResourceGroupName vh-automation-dev -AutomationAccountName vh-automation-dev -SourcePath 'C:\source\vh-windows-build-agent\FileResourceDemo.ps1' -Verbose
