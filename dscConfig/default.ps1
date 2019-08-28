3de4693de95dbe954b4ed61bcf67401ec879a91d
janisBerz <janis.berzins@hmcts.net>
2019-08-28 09:30:36 +0300

    Adding custom script extension updating node config

--- dscConfig/default.ps1
+++ dscConfig/default.ps1
@@ -19,29 +19,6 @@ Configuration default {
             DestinationPath = "C:\temp"
         }
 
-        # Script DownloadExe {
-        #     GetScript  =
-        #     {
-        #         @{
-        #             GetScript  = $GetScript
-        #             SetScript  = $SetScript
-        #             TestScript = $TestScript
-        #             Result     = ('True' -in (Test-Path "C:\temp\vs_buildtools.exe"))
-        #         }
-        #     }
-
-        #     SetScript  =
-        #     {
-        #         Invoke-WebRequest -Uri "https://aka.ms/vs/15/release/vs_buildtools.exe" -OutFile "C:\temp\vs_buildtools.exe"
-        #     }
-
-        #     TestScript =
-        #     {
-        #         $Status = ('True' -in (Test-Path "C:\temp\vs_buildtools.exe"))
-        #         $Status -eq $True
-        #     }
-        # }
-
         Script javaSDK {
             GetScript  =
             {
@@ -81,52 +58,6 @@ Configuration default {
             }
         }
 
-        # VSInstall VisualStudio2017 {
-        #     ExecutablePath = "C:\temp\vs_buildtools.exe"
-        #     Workloads      = "Microsoft.VisualStudio.Workload.MSBuildTools", "Microsoft.Net.Core.Component.SDK", "Microsoft.Net.ComponentGroup.4.6.2.DeveloperTools", "Microsoft.VisualStudio.Workload.WebBuildTools", "Microsoft.VisualStudio.Workload.NodeBuildTools", "Microsoft.Net.Component.3.5.DeveloperTools", "Microsoft.VisualStudio.Component.TestTools.BuildTools", "Microsoft.VisualStudio.Component.TypeScript.2.8", "Microsoft.VisualStudio.Component.TestTools.BuildTools", "Microsoft.VisualStudio.Workload.MSBuildTools"
-        #     Ensure         = 'Present'
-        #     InstallAccount = $patCredential
-        # }
-
-        # Script java {
-        #     GetScript  =
-        #     {
-        #         @{
-        #             GetScript  = $GetScript
-        #             SetScript  = $SetScript
-        #             TestScript = $TestScript
-        #             Result     = ('True' -in (Test-Path "c:\Program Files\Java\jdk1.8.0_131\bin"))
-        #         }
-        #     }
-
-        #     SetScript  =
-        #     {
-        #         ### Download and Install Java
-        #         Write-Output "Installing Java"
-        #         #Set-ExecutionPolicy Unrestricted -Force
-        #         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-        #         #Default workspace location
-        #         Set-Location C:\
-        #         $source = "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-windows-x64.exe"
-        #         $destination = "D:\jdk-8u131-windows-x64.exe"
-        #         $client = new-object System.Net.WebClient
-        #         $cookie = "oraclelicense=accept-securebackup-cookie"
-        #         $client.Headers.Add([System.Net.HttpRequestHeader]::Cookie, $cookie)
-        #         $client.downloadFile($source, $destination)
-        #         $proc = Start-Process -FilePath $destination -ArgumentList "/s" -Wait -PassThru
-        #         $proc.WaitForExit()
-        #         [System.Environment]::SetEnvironmentVariable("JAVA_HOME", "c:\Program Files\Java\jdk1.8.0_131", "Machine")
-        #         [System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";c:\Program Files\Java\jdk1.8.0_131\bin", "Machine")
-        #         $Env:Path += ";c:\Program Files\Java\jdk1.8.0_131\bin"
-        #     }
-
-        #     TestScript =
-        #     {
-        #         $Status = ('True' -in (Test-Path "c:\Program Files\Java\jdk1.8.0_131\bin"))
-        #         $Status -eq $True
-        #     }
-        # }
- 
         Script VSBuildTools2019 {
             # Must return a hashtable with at least one key            
             # named 'Result' of type String            
@@ -222,7 +153,7 @@ Configuration default {
                 ServerUrl         = 'https://hmctsreform.visualstudio.com'
                 Pool              = 'vh-pool-dev'
                 AccountCredential = $patCredential
-                AgentDirectory    = 'C:\VSTSAgent'+ $i
+                AgentDirectory    = 'F:\VSTSAgent'+ $i
                 Ensure            = 'Present'
             }
         }
