cfcc0b8d543231bfb58aa2ced4492f061f314164
janisBerz <janis.berzins@hmcts.net>
2019-09-04 16:23:32 +0300

    Updated script

--- helperScripts/Add-AzAutomationCertificate.ps1
+++ helperScripts/Add-AzAutomationCertificate.ps1
@@ -1,21 +1,25 @@
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
     $certificateName,
 
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
     $certificatePassword,
 
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
     $certificatePath,
 
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
     $automationAccountName
 )
 
-$Password = ConvertTo-SecureString -String $certificatePassword -AsPlainText -Force
-New-AzAutomationCertificate -AutomationAccountName $automationAccountName -Name $certificateName -Path $certificatePath -Password $Password -ResourceGroupName $automationAccountName
+$password = ConvertTo-SecureString -String $certificatePassword -AsPlainText -Force
+
+$cert = Get-AzAutomationCertificate -AutomationAccountName $automationAccountName -Name $certificateName -ResourceGroupName $automationAccountName
+if (!$cert) {
+    New-AzAutomationCertificate -AutomationAccountName $automationAccountName -Name $certificateName -Path $certificatePath -Password $password -ResourceGroupName $automationAccountName
+}
