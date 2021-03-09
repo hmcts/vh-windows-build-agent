ceaf64d0744dca764f601f88ddd82206363307fd
karanotts <nottinghamkara@gmail.com>
2021-03-09 09:10:04 +0000

    null - choco

---
+++ terraform/60-null_resource_chocolatey.tf
@@ -0,0 +1,19 @@
+resource "null_resource" "chocolatey" {
+  depends_on = [azurerm_virtual_machine.buildagent]
+  # triggers = {
+  #   always_run = timestamp()
+  # }
+  provisioner "remote-exec" {
+    connection {
+      type     = "winrm"
+      user     = random_password.username.result
+      password = random_password.password.result
+      host     = azurerm_public_ip.buildagent.ip_address
+    }
+
+    inline = [
+      "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+    ]
+
+  }
+}
