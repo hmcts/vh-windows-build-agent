12507febe1b19732d29f1b9a5df5822217bade74
karanotts <nottinghamkara@gmail.com>
2021-03-10 14:29:57 +0000

    run pwsh

--- terraform/60-null_resource_chocolatey.tf
+++ terraform/60-null_resource_chocolatey.tf
@@ -13,7 +13,7 @@ resource "null_resource" "chocolatey" {
     }
 
     inline = [
-      "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+      "powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
     ]
 
   }
